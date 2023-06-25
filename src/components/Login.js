78388bd48cde667d089fd322c8240d653d4330ce
Gulperi Ozbulut <gulperiozblt@gmail.com>
2023-06-25 19:13:00 +0300

    Completed assignment

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,65 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
+function NavBar({ onLogout }) {
+    const history = useHistory();
+
+    function handleClick() {
+        // logout the user
+        onLogout();
+        // then navigate them to the login page
+        history.push("/login");
+    }
+
+    return (
+        <nav>
+            <button onClick={handleClick}>Logout</button>
+        </nav>
+    );
+}
+
+
 function Login({ setIsLoggedIn }) {
-  const history = useHistory();
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
-
-  function handleChange(e) {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
+    const history = useHistory();
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
     });
-  }
 
-  function handleSubmit(e) {
-    e.preventDefault();
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    }
 
-    setIsLoggedIn(true);
+    function handleSubmit(e) {
+        e.preventDefault();
 
-    // after logging the user in, redirect to the home page!
-    history.push("/");
-  }
+        setIsLoggedIn(true);
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
-      <input
-        type="text"
-        name="username"
-        value={formData.username}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-      />
-      <button type="submit">Login</button>
-    </form>
-  );
+        // after logging the user in, redirect to the home page!
+        history.push("/");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <input
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+            />
+            <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
 }
 
 export default Login;
